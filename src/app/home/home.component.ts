3d85c49582172b08adadd4e9b6f557c709b755c6
mbaye diop <mbaye.diop@suite.sn>
2018-01-16 17:41:57 +0000

    creation and configuration of home  page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {HomeService} from "./home.service";
+import 'rxjs/Rx';
+import {DataService} from "../data.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers : [HomeService]
+})
+export class HomeComponent implements OnInit {
+
+  user={
+    adresse: "",
+    age:0,
+    email:"",
+    matricule: "",
+    nom: "",
+    prenom: "",
+    sexe: "",
+    telephone: "",
+    username: "",
+  };
+
+  constructor(private router: Router, private dataService: DataService, private homeService : HomeService) { }
+
+  ngOnInit() {
+    this.user = this.dataService.getUser();
+    console.log(this.user);
+  }
+
+  deconnexion()
+  {
+    this.dataService.deconnect();
+    this.router.navigate(['']);
+  }
+
+}
