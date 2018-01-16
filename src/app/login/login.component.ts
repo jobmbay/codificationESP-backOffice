675281db59ae243fe1784c1a3edd7e631cc8b800
mbaye diop <mbaye.diop@suite.sn>
2018-01-16 17:38:27 +0000

    creation and configuration of login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from "../data.service";
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = {
+    username: "",
+    password: ""
+  }
+
+  constructor(private router: Router, private dataService : DataService) { }
+
+  ngOnInit() {
+  }
+
+  login()
+  {
+    this.dataService.login(this.user).then(
+      data=>{
+        this.dataService.setUser(data["user"]);
+        this.dataService.setTocken(data["id"]);
+        this.router.navigate(['home/dashboard']);
+      },
+      err=> {
+        this.user.password="";
+        console.log(err)
+      }
+    );
+  }
+
+}
