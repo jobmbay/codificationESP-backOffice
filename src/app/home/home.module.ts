95fd2596bc880818279b49384ae7115cfd9212ab
mbaye diop <mbaye.diop@suite.sn>
2018-01-16 17:37:02 +0000

    creation and configuration of home modules

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {HomeRouting} from "./home.routing";
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { ProjetsComponent } from './projets/projets.component';
+import { ProjetDetailsComponent } from './projets/projet-details/projet-details.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    HomeRouting,
+    Ng2SmartTableModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [ProjetsComponent, ProjetDetailsComponent]
+})
+export class HomeModule { }
